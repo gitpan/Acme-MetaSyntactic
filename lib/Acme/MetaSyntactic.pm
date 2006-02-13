134094ed7505cc3ae1c763b09060ec8c3d0bcfdc
Philippe Bruhat (BooK) <book@cpan.org>
2006-02-13 01:14:29 +0000

    Import of BOOK/Acme-MetaSyntactic-0.61 from CPAN.
    gitpan-cpan-distribution: Acme-MetaSyntactic
    gitpan-cpan-version:      0.61
    gitpan-cpan-path:         BOOK/Acme-MetaSyntactic-0.61.tar.gz
    gitpan-cpan-author:       BOOK
    gitpan-cpan-maturity:     released

--- lib/Acme/MetaSyntactic.pm
+++ lib/Acme/MetaSyntactic.pm
@@ -7,7 +7,7 @@ use File::Basename;
 use File::Spec;
 use File::Glob;
 
-our $VERSION = '0.60';
+our $VERSION = '0.61';
 
 # some class data
 our $Theme = 'foo'; # default theme
@@ -414,7 +414,7 @@ C<Acme::MetaSyntactic::Locale>), or a new one of your invention).
 
 Individual contributors are listed in the individual theme files.
 Look at the included F<CONTRIBUTORS> file for the list of all
-contributors (32 in this version).
+contributors (33 in this version).
 
 However, this module could not have been possible without:
 
